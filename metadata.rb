d1ffd5bb06ac9d3bc723b70d0db2035396891b03
Eric Ashman <eashman@drfirst.com>
2014-04-23 18:41:06 -0400

    upped version #

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'aaron@duedil.com'
 license          'Apache 2.0'
 description      'Installs Graylog2 and its dependencies'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 depends           'java'
